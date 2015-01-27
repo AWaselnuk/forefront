93ca2434b6db067302f1cbd49ea4e1cb2bd05776
Adam Waselnuk <awaselnuk@mygrocerydeals.com>
2015-01-27 09:21:30 -0500

    Recursive file copy example.

--- forefront.js
+++ forefront.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
 'use strict';
-console.log('Hello Forefront!');
 
 var program = require('commander'),
-    chalk = require('chalk');
+    chalk = require('chalk'),
+    fs = require('fs-extra');
+
+console.log(chalk.bold.white.bgCyan('Welcome to Forefront!'));
+
+//  UI themes
+
+// File paths
+var appDir = __dirname + '/';
+var workingDir = './';
+
+// Copy SASS Templates
+// wrench.mkdirSyncRecursive(workingDir + 'src/scss');
+// wrench.copyDirSyncRecursive(appDir + 'templates/src/scss', workingDir + 'src/scss');
+fs.ensureDirSync(workingDir + 'src/scss');
+fs.copySync(appDir + 'templates/src/scss', workingDir + 'src/scss');
